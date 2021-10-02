e82dd8705a46b58ed38e4fbe29e83e3a6417bea0
David Couderc <david@debian.localdomain>
2021-10-03 01:21:26 +0200

    update virtualfish

--- debian.fish
+++ debian.fish
@@ -1,9 +1,6 @@
-if status --is-interactive
-    eval (python -m virtualfish auto_activation)
-end
-
-set PATH $PATH /opt/node/bin/
+set PATH $PATH /opt/node/bin/ $HOME/.pyenv/bin/
 
 alias tvoff "xrandr --output HDMI-A-0 --off"
 alias tvon "xrandr --output HDMI-A-0 --right-of DisplayPort-0 --auto"
+alias duplicate "xrandr --output HDMI-A-0 --mode 1360x768 --same-as DisplayPort-0 --output DisplayPort-0 --scale-from 1360x768"
 alias pyenv $HOME/.pyenv/bin/pyenv
